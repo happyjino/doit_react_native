4f256963f54bf04690fa8c79c3d22f02d551e45c
happyjino <wlsgh1128@naver.com>
2023-01-19 00:34:23 +0900

    ch02_3

--- App.tsx
+++ App.tsx
@@ -1,6 +1,20 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView, ScrollView} from 'react-native';
+import ArrowComponent from './src/screens/ArrowComponent';
+import ClassComponent from './src/screens/ClassComponent';
+import Person from './src/screens/Person';
+import * as D from './src/data';
+
+const person = D.createRandomPerson();
+const people = D.makeArray(100).map(D.createRandomPerson);
 
 export default function App() {
-  return <SafeAreaView />;
+  const children = people.map(person => (
+    <Person key={person.id} person={person} />
+  ));
+  return (
+    <SafeAreaView>
+      <ScrollView>{children}</ScrollView>
+    </SafeAreaView>
+  );
 }
