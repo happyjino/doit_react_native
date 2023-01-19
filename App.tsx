792d65b28839bdcd997c914caed877d103ffa118
happyjino <wlsgh1128@naver.com>
2023-01-19 10:44:06 +0900

    ch03_2

--- App.tsx
+++ App.tsx
@@ -1,30 +1,31 @@
-// ch03_1
+// ch03_2
 
 import React from 'react';
-import {SafeAreaView, Text, StyleSheet} from 'react-native';
+// prettier-ignore
+import {SafeAreaView, Text,StyleSheet, Platform, Dimensions, View} from 'react-native';
 import {MD2Colors} from 'react-native-paper';
 import Color from 'color';
 
-console.log(MD2Colors.blue500);
-console.log(Color(MD2Colors.blue500).alpha(0.5).lighten(0.5).string());
+const {width, height} = Dimensions.get('window');
 
+// prettier-ignore
 export default function App() {
   return (
     <SafeAreaView style={styles.safeAreaView}>
-      <Text style={[{color: 'white'}]}>Hello StyleSheet World!</Text>
+      <Text style={[styles.text]}>os: {Platform.OS}</Text>
+      <Text style={[styles.text]}>width: {width}</Text>
+      <Text style={[styles.text]}>height: {height}</Text>
+
+      <View style={[styles.box, {borderRadius: 10}]} />
+      <View style={[styles.box, styles.border]} />
+      <View style={[styles.box, styles.border, {borderRadius: 10}]} />
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  safeAreaView: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: MD2Colors.blue500,
-  },
-  text: {
-    fontSize: 20,
-    color: Color(MD2Colors.blue500).alpha(0.7).lighten(0.9).string(),
-  },
+// prettier-ignore
+const styles= StyleSheet.create({
+  safeAreaView: { backgroundColor: MD2Colors.blue500, flex: 1, paddingLeft: Platform.select({ios: 0, android: 20})},
+  text: { fontSize: 20, color: Color(MD2Colors.blue500).lighten(0.9).string(), marginBottom: 10, marginLeft: 50, marginTop: 20 },
+  box: { height: 100, width: '70%', backgroundColor: 'white', marginBottom: 10, marginLeft: Platform.select({ ios: 20, android: 0}) },
+  border: { borderWidth: 10, borderColor: MD2Colors.lime500}
 });
