d81ce671f2b0e1d73d97a03ed71725cbb6d27e55
happyjino <wlsgh1128@naver.com>
2023-04-05 21:46:39 +0900

    ch05_1 완료

--- App.tsx
+++ App.tsx
@@ -1,20 +1,27 @@
 // ch05_1
-import React from 'react';
-import { SafeAreaView, StyleSheet } from 'react-native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import React, {useState, useCallback} from 'react';
+import { SafeAreaView, StyleSheet, useColorScheme } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
+import { DefaultTheme, MD3DarkTheme } from 'react-native-paper';
 import MainNavigator from './src/screens/MainNavigator';
+import { ToggleThemeProvider } from './src/contexts';
 
 export default function App() {
+  const scheme = useColorScheme();
+  const [theme, setTheme] = useState(scheme == 'dark' ? MD3DarkTheme : DefaultTheme)
+  const toggleTheme = useCallback(() => setTheme((theme) => (theme.dark ? DefaultTheme : MD3DarkTheme)), [])
+  
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={[styles.safeAreaView]}>
-        <MainNavigator />
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <PaperProvider theme={theme}>
+      <ToggleThemeProvider toggleTheme={toggleTheme}>
+        <SafeAreaView style={styles.safeAreaView}>
+          <MainNavigator />
+        </SafeAreaView>
+      </ToggleThemeProvider>
+    </PaperProvider>
   )
 }
 
-// prettier-ignore
 const styles = StyleSheet.create({
   safeAreaView: { flex: 1 },
 });
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,18 +1,38 @@
 import React from 'react';
 import {StyleSheet, View, Text} from 'react-native';
-import {MD2Colors} from 'react-native-paper';
-import * as D from '../data';
+import { Switch, useTheme } from 'react-native-paper';
+import { useToggleTheme } from '../contexts';
 
-const title = 'CopyMe';
-export default function CopyMe() {
+export default function Home() {
+  const {dark, colors, fonts} = useTheme();
+  const toggleTheme = useToggleTheme();
+  
   return (
-    <View style={[styles.view]}>
-      <Text style={[styles.text]}>{title}</Text>
+    <View style={[styles.view, { backgroundColor: colors.background }]}>
+      <View style={[styles.bar, { backgroundColor: colors.primary }]}>
+        <Text style={[styles.text, { color: colors.onSurface, fontWeight: '500' }]}>
+          TopBar
+        </Text>
+        <View style={[{ flex: 1 }]} />
+        <Switch value={dark} onValueChange={toggleTheme} />
+      </View>
+      <View style={styles.content}>
+        <Text style={[styles.text, { color: colors.onSurface, fontWeight: '500' }]}>
+          Welcome to Context world!
+        </Text>
+      </View>
+      <View style={[styles.bar, { backgroundColor: colors.secondary }]}>
+        <Text style={[styles.text, { color: colors.onSurface, fontWeight: '500' }]}>
+          BottomBar
+        </Text>
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  view: {flex: 1, padding: 5, backgroundColor: MD2Colors.blue900},
-  text: {fontSize: 20, color: 'white'},
+  view: { flex: 1 },
+  bar: { height: 50, flexDirection: 'row', padding: 5, alignItems: 'center' },
+  content: { flex: 1, alignItems: 'center', justifyContent: 'center' },
+  text: {fontSize: 20, textAlign: 'center'},
 });
